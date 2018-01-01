dc3457c72b2768f47c4dc58890d30727fe3d3e02
Alan <ivda.calderon@gmail.com>
2018-01-01 16:48:32 -0300

    second commit

---
+++ Models/Persona.cs
@@ -0,0 +1,26 @@
+/// <summary>
+/// Archivo donde se definen las clases del Dominio del problema.
+/// </summary>
+namespace Cotizaciones.Models {
+
+    /// <summary>
+    /// Clase que representa a una persona en el Sistema.
+    /// </summary>
+    /// <remarks>
+    /// Esta clase pertenece al modelo del Dominio y posee las siguientes restricciones:
+    /// - No permite valores null en sus atributos.
+    /// </remarks>
+    public class Persona
+    {
+        public int Id { get; set; }
+
+        public string Rut { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Paterno { get; set; }
+
+        public string Materno { get; set; }
+
+    }
+}
